f23f2b4a00632cd41aeb159451732cd971f9ea82
ellamurii <emb.baguio@gmail.com>
2023-11-28 17:52:26 +0800

    morgan

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
